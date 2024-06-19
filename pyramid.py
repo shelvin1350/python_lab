b9b194207c805f561839516dfdd2f94ff4fe883d
shelvin sunil <shelvinsunilphilip@gmail.com>
2024-06-19 15:00:59 +0530

    commit

--- pyramid.py
+++ pyramid.py
@@ -1,1 +1,14 @@
-input("Enter the limit: ")
+num_rows = int(input("Enter the number of rows for the pyramid: "))
+
+for i in range(1, num_rows + 1):
+    
+    for j in range(num_rows - i):
+        print(" ", end="")
+
+    for k in range(1, i + 1):
+        print(k, end="")
+    
+    for l in range(i - 1, 0, -1):
+        print(l, end="")
+
+    print()
